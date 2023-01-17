57385677c25fc65eab9d66da88274d5ce3b7796e
azizbenyaala98 <azizbenyaala@gmail.com>
2023-01-17 20:43:05 +0100

    feat update

--- src/main/java/com/tekupminiproject/TekupMiniProject/ImmobilierApplication.java
+++ src/main/java/com/tekupminiproject/TekupMiniProject/ImmobilierApplication.java
@@ -1,13 +1,26 @@
 package com.tekupminiproject.TekupMiniProject;
 
+import jdk.internal.dynalink.support.NameCodec;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ImmobilierApplication {
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ImmobilierApplication.class, args);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println("encoder:  " + encoder.encode("admin"));
 	}
 
 	}
